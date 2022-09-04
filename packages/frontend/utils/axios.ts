3aa483310a443088af228254757938d1269b64ed
tictac <fominsartems947@gmail.com>
2022-09-04 03:13:18 +0300

    fix

--- packages/frontend/utils/axios.ts
+++ packages/frontend/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 //http://localhost:5000
-export const API_URL = "http://localhost:5000"
+//fullstack-reddit-clone-production.up.railway.app
+export const API_URL = "fullstack-reddit-clone-production.up.railway.app"
 
 export const $api = axios.create({
 	baseURL: API_URL,
