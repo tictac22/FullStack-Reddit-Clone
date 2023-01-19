52d13b9cad7d64f5cb9d312e8018520c938121ec
tictac <fominsartems947@gmail.com>
2023-01-19 22:41:25 +0200

    commit

--- packages/frontend/utils/axios.ts
+++ packages/frontend/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 //http://localhost:3333
 //https://fullstack-reddit-clone-production.up.railway.app/
-export const API_URL = "http://localhost:3333"
+export const API_URL = "https://fullstack-reddit-clone-production.up.railway.app/"
 //eslint-disable-next-line
 console.log(API_URL)
 export const $api = axios.create({
